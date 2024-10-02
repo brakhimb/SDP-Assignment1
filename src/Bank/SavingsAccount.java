67e771c74ad038e3d14a9fbe94f36c8345e7fffb
brakhimb <rahimbaevbekzhan@gmail.com>
2024-10-02 19:40:57 +0500

    Third commit

--- src/Bank/SavingsAccount.java
+++ src/Bank/SavingsAccount.java
@@ -1,15 +1,15 @@
-package Bank;
-
-public class SavingsAccount extends Account {
-    private final double RATING = 1.3;
-    public SavingsAccount (double balance) {
-        this.balance = balance;
-    }
-    void updateBalance(double amount) {
-        balance += amount * RATING;
-    }
-    public double getBalance () {
-        return balance;
-    }
-}
-
+package Bank; 
+ 
+public class SavingsAccount extends Account {  
+    private final double RATING = 1.3;  
+    public SavingsAccount (double balance) {   
+        this.balance = balance;  
+    }  
+    void updateBalance(double amount) {  
+        balance += amount * RATING;  
+    }  
+    public double getBalance () {  
+        return balance; 
+    }  
+} 
+ 
